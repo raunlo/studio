446be44baa74b147be610225e50675e8d3b88902
Rauno Lõhmus <32277590+raunlo@users.noreply.github.com>
2025-08-26 01:16:18 +0200

    Enable HTTP keep-alive for Axios requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,16 @@
 
 import Axios, { AxiosError, AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
+import http from 'http';
+import https from 'https';
 
 // The base URL should always be the relative path to our proxy.
 const NEXT_PUBLIC_API_BASE_URL = '/api/proxy';
 
-const axiousProps = {
-  baseURL: NEXT_PUBLIC_API_BASE_URL
-}
+const axiousProps: AxiosRequestConfig = {
+  baseURL: NEXT_PUBLIC_API_BASE_URL,
+  httpAgent: new http.Agent({ keepAlive: true }),
+  httpsAgent: new https.Agent({ keepAlive: true }),
+};
 // The base URL is now the local proxy endpoint.
 const axiosInstance = Axios.create(axiousProps as CreateAxiosDefaults);
 
